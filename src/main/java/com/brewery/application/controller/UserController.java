df45a0b16d22da0d39abfb133192bf993e6ebf95
kameshSarvan <venkatakamesh.sarvan@thoughtclan.com>
2023-08-22 17:03:42 +0530

    user service

--- src/main/java/com/brewery/application/controller/UserController.java
+++ src/main/java/com/brewery/application/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.brewery.application.controller;
 
 import com.brewery.application.dto.inputdto.UserInDto;
+import com.brewery.application.dto.outputdto.UserOutDto;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController{
 
-    public UserInDto
+    public UserOutDto createUser(UserInDto user){
+        return null;
+    }
 }
