af7b6de6e5c80fdd93ca189e3f5b742b85905182
SitaRam-1287 <sitarami.reddy@thoughtclan.com>
2023-08-23 12:50:33 +0530

    Changes in item related stuff

--- src/main/java/com/brewery/application/entity/Item.java
+++ src/main/java/com/brewery/application/entity/Item.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.UUID;
 
--- src/main/java/com/brewery/application/service/Impl/ItemServiceImpl.java
+++ src/main/java/com/brewery/application/service/Impl/ItemServiceImpl.java
@@ -2,43 +2,65 @@ package com.brewery.application.service.Impl;
 
 import com.brewery.application.dto.inputdto.ItemInDto;
 import com.brewery.application.dto.outputdto.ItemOutDto;
-import com.brewery.application.fascade.ItemFascade;
+import com.brewery.application.entity.Item;
+import com.brewery.application.repository.ItemRepository;
+import com.brewery.application.service.ItemService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 
 @Service
-public class ItemServiceImpl {
+public class ItemServiceImpl implements ItemService {
     @Autowired
-    private ItemFascade itemFascade;
+    private ItemRepository itemRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+    @Override
     public ItemOutDto createItem(ItemInDto item) {
-        return itemFascade.createItem(item);
+        Item item1=modelMapper.map(item,Item.class);
+        Item saveItem=itemRepository.save(item1);
+        return modelMapper.map(saveItem,ItemOutDto.class);
     }
 
+    @Override
     public ItemOutDto getItem(UUID id) {
-        return itemFascade.getItem(id);
+        Item getItem=itemRepository.findById(id).orElseThrow(()->new RuntimeException("Item with id not found"));
+        return modelMapper.map(getItem,ItemOutDto.class);
     }
 
+    @Override
     public Collection<ItemOutDto> getAllItems() {
-        return itemFascade.getAllItems();
+        Collection<Item> getAllItems=itemRepository.findAll();
+        return Collections.singleton(modelMapper.map(getAllItems, ItemOutDto.class));
     }
 
+    @Override
     public ItemOutDto updateItem(ItemInDto item) {
-        return itemFascade.updateItem(item);
+        Item item1=modelMapper.map(item,Item.class);
+        Item existingItem=itemRepository.findById(item1.getId()).orElseThrow(()->new RuntimeException("Item with id not found"));
+        modelMapper.map(item1,existingItem);
+        Item saveItem=itemRepository.save(existingItem);
+        return modelMapper.map(saveItem,ItemOutDto.class);
     }
 
+    @Override
     public ItemOutDto patchItem(ItemInDto item) {
-        return itemFascade.patchItem(item);
+        return null;
     }
 
+    @Override
     public void deleteItem(UUID id) {
-        itemFascade.deleteItem(id);
+        itemRepository.deleteById(id);
     }
 
+    @Override
     public void deleteAllItems() {
-        itemFascade.deleteAllItems();
+        itemRepository.deleteAll();
     }
+
 }
